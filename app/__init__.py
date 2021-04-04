4eb917e216f4b6f36efc009870f9e4a2c9a197b2
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-04-04 18:20:13 -0300

    config: definição inicial da rota olá mundo

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,4 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 app.config.from_object('config')
 
-
-@app.route('/', methods=['GET'])
-def root():
-    return jsonify({'message': 'Olá!!'})
+from .routes import routes
